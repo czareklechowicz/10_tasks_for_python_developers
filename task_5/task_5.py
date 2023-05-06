4b5e47ff5269c0675d73dc41f4462a90538bedea
czareklechowicz <czareklechowicz2000@wp.pl>
2023-05-06 14:34:49 +0200

    task 5 adding lower() funkction
    Signed-off-by: czareklechowicz <czareklechowicz2000@wp.pl>

--- task_5/task_5.py
+++ task_5/task_5.py
@@ -3,8 +3,7 @@ names = [x.strip() for x in user_input.split(",")]
 
 names_with_a = []
 for name in names:
-    if name.startswith('A'):
+    if name.lower().startswith('a'):  # porównanie z małą literą "a"
         names_with_a.append(name)
-
 
 print("Imiona zaczynające się na literę A to:", names_with_a)
